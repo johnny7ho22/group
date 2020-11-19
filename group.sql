a49df6a7cbc2bfaf49a0bc02f32929a84f9df524
何政賢 <johnny7ho22@gmail.com>
2020-11-19 13:56:05 +0800

    最新

--- group.sql
+++ group.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： localhost:8889
--- 產生時間： 2020 年 11 月 19 日 05:53
+-- 產生時間： 2020 年 11 月 19 日 05:55
 -- 伺服器版本： 5.7.30
 -- PHP 版本： 7.4.9
 
@@ -13,6 +13,27 @@ SET time_zone = "+00:00";
 --
 -- 資料庫： `group`
 --
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `apply`
+--
+
+CREATE TABLE `apply` (
+  `id` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `number` int(100) NOT NULL,
+  `father` varchar(100) NOT NULL,
+  `mother` varchar(100) NOT NULL,
+  `type` int(11) NOT NULL,
+  `teacher_comment` varchar(100) NOT NULL,
+  `teacher_name` varchar(100) NOT NULL,
+  `result` varchar(100) NOT NULL,
+  `secretary_comment` varchar(100) NOT NULL,
+  `secretary_name` varchar(100) NOT NULL,
+  `principal_name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -41,6 +62,12 @@ INSERT INTO `user` (`id`, `LoginID`, `password`) VALUES
 --
 
 --
+-- 資料表索引 `apply`
+--
+ALTER TABLE `apply`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 資料表索引 `user`
 --
 ALTER TABLE `user`
@@ -49,6 +76,12 @@ ALTER TABLE `user`
 --
 -- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
 --
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `apply`
+--
+ALTER TABLE `apply`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `user`
