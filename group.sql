dee13127d9301064b4b748579bb74b715be0bef6
何政賢 <johnny7ho22@gmail.com>
2020-11-19 20:34:46 +0800

    最新版本

--- group.sql
+++ group.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： localhost:8889
--- 產生時間： 2020 年 11 月 19 日 05:55
+-- 產生時間： 2020 年 11 月 19 日 12:33
 -- 伺服器版本： 5.7.30
 -- PHP 版本： 7.4.9
 
@@ -26,14 +26,23 @@ CREATE TABLE `apply` (
   `number` int(100) NOT NULL,
   `father` varchar(100) NOT NULL,
   `mother` varchar(100) NOT NULL,
-  `type` int(11) NOT NULL,
-  `teacher_comment` varchar(100) NOT NULL,
-  `teacher_name` varchar(100) NOT NULL,
-  `result` varchar(100) NOT NULL,
-  `secretary_comment` varchar(100) NOT NULL,
-  `secretary_name` varchar(100) NOT NULL,
-  `principal_name` varchar(100) NOT NULL
+  `type` varchar(30) NOT NULL,
+  `teacher_comment` varchar(100) DEFAULT NULL,
+  `teacher_name` varchar(100) DEFAULT NULL,
+  `result` varchar(100) DEFAULT NULL,
+  `secretary_comment` varchar(100) DEFAULT NULL,
+  `secretary_name` varchar(100) DEFAULT NULL,
+  `principal_name` varchar(100) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- 傾印資料表的資料 `apply`
+--
+
+INSERT INTO `apply` (`id`, `name`, `number`, `father`, `mother`, `type`, `teacher_comment`, `teacher_name`, `result`, `secretary_comment`, `secretary_name`, `principal_name`, `status`) VALUES
+(1, '何政賢', 106213015, '爸', '媽', '低收入戶', '導師的意見', '林書豪', '為低收入戶', '很可憐', '我是秘書', 'chen', 0),
+(3, '陳', 1062345, '王', '林', '中低收入戶', NULL, NULL, NULL, NULL, NULL, NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -81,7 +90,7 @@ ALTER TABLE `user`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `apply`
 --
 ALTER TABLE `apply`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `user`
